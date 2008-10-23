064bd68fa1a0f07eb7f2b748d4028fc5dbab64bd
Bob Aman <bob@sporkmonger.com>
2008-10-23 14:54:55 -0400

    Updated version number.

--- lib/instrument/version.rb
+++ lib/instrument/version.rb
@@ -27,7 +27,7 @@ unless defined? Instrument::VERSION
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 1
-      TINY  = 2
+      TINY  = 3
 
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
